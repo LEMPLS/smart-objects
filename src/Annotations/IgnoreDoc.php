2a5ec636443b3f55ede03b4f1e96f760e5b08deb
Martin Kapal <flamecze@gmail.com>
2017-01-09 15:46:17 +0100

    ignoredoc annotation fix

--- src/Annotations/IgnoreDoc.php
+++ src/Annotations/IgnoreDoc.php
@@ -11,7 +11,55 @@ namespace Lempls\SmartObjects\Annotations;
 
 use phpDocumentor\Reflection\DocBlock\Tags\BaseTag;
 
-abstract class IgnoreDoc extends BaseTag
+/**
+ * Class IgnoreDoc
+ *
+ * @Annotation
+ * @package Lempls\SmartObjects\Annotations
+ */
+class IgnoreDoc extends BaseTag
 {
+
+    /** @var string Name of the tag */
+    protected $name = 'ignoreDoc';
+
+    /** @var Description|null Description of the tag. */
+    protected $description;
+
+    /**
+     * Gets the name of this tag.
+     *
+     * @return string The name of this tag.
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function render(\phpDocumentor\Reflection\DocBlock\Tags\Formatter $formatter = null)
+    {
+        if ($formatter === null) {
+            $formatter = new Formatter\PassthroughFormatter();
+        }
+
+        return $formatter->format($this);
+    }
+
+
+    public static function create($body)
+    {
+        parent::create($body);
+    }
+
+
+    public function __toString()
+    {
+        return parent::__toString();
+    }
 
 }
